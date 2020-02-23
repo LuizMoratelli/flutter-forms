132da62a7a18e2b4d2b8e1404429b41585bb5a72
Luiz Augusto Moratelli <gameplayteam12@gmail.com>
2020-02-23 20:51:06 -0300

    feat(form): separate inputs in individual classes

--- lib/screens/form.dart
+++ lib/screens/form.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart' as m;
 import 'package:forms/widgets/text_form_field.dart';
-import 'package:multiselect_formfield/multiselect_formfield.dart';
 import 'package:forms/utils/validator.dart';
 import 'package:forms/theme.dart';
+import 'package:forms/widgets/radio_form_field.dart';
+import 'package:forms/widgets/date_form_field.dart';
+import 'package:forms/widgets/multiselect_form_field.dart';
 
 class Form extends m.StatefulWidget {
   final formKey;
@@ -28,46 +30,27 @@ class _FormState extends m.State<Form> {
             labelText: 'Endereço de e-mail',
             validator: Validator.email,
           ),
-          m.GestureDetector(
-            onTap: () async => await m.showDatePicker(
-              context: context,
-              initialDate: DateTime.now(),
-              firstDate: DateTime(1900),
-              lastDate: DateTime.now(),
-              builder: (m.BuildContext context, m.Widget child) {
-                return m.Theme(
-                  data: m.ThemeData.dark().copyWith(
-                    primaryColor: ThemeColors.green,
-                    accentColor: ThemeColors.green,
-                  ),
-                  child: child,
-                );
-              },
-            ),
-            child: m.AbsorbPointer(
-              child: TextFormField(labelText: 'Data de Fundação'),
-            ),
+          DateFormField(
+            'Data de fundação',
+            initialDate: DateTime.now(),
           ),
           TextFormField(labelText: 'Setor de atividade'),
           TextFormField(labelText: 'Número aproximado de empregados'),
-          MultiSelectFormField(
-            titleText: 'Qual é o seu entendimento acerca de inovação',
-            validator: (value) {
-              if (value == null || value.length == 0) {
-                return 'Uma ou mais opções devem ser selecionadas';
-              }
-            },
-            dataSource: [
-              {
-                'display': 'Teste',
-                'value': 'teste',
-              },
+          RadioFormField(
+            'Teste',
+            groupValue: 't',
+            items: [
+              RadioItem(
+                't1',
+                value: '1',
+              ),
             ],
-            textField: 'display',
-            valueField: 'value',
-            okButtonLabel: 'Ok',
-            cancelButtonLabel: 'Cancel',
-            hintText: 'Por favor, selecione uma ou mais opções',
+          ),
+          MultiSelectFormField(
+            'Teste2',
+            items: [
+              MultiSelectItem('T2', value: 't2'),
+            ],
           ),
         ],
       ),
